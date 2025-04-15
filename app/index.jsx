7f02dd73c7dd57c073a3e963ffdbebdd7c14169f
zengks <zengks@outlook.com>
2025-04-15 14:48:13 -0700

    end of lession 2

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import Logo from "../assets/img/logo_light.png";
 
 const index = () => {
     return (
-        <View>
-            <Text>The Number 1</Text>
-            <Text>Reading List App</Text>
+        <View style={styles.container}>
+            <Image source={Logo} style={styles.img} />
+            <Text style={styles.title}>The Number 1</Text>
+            <Text style={{ marginTop: 10, marginBottom: 30 }}>
+                Reading List App
+            </Text>
         </View>
     );
 };
 
 export default index;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 18,
+    },
+    img: {
+        marginVertical: 20,
+    },
+});
