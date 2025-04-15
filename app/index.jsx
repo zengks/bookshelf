63aef59eb54a50523f6c8de586932fed8c67ba91
zengks <zengks@outlook.com>
2025-04-15 12:44:12 -0700

    created basic components

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const index = () => {
+    return (
+        <View>
+            <Text>The Number 1</Text>
+            <Text>Reading List App</Text>
+        </View>
+    );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
